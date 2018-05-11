496e97b75c1898ca9c63e7bcef856a2bf4f56479
Harshit Khanna <harshitkhanna3254@gmail.com>
2018-05-11 16:42:49 +0530

    Port change mistake updated

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 var app = express();
-
+var server = http.createServer(app);
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -11,7 +13,7 @@ app.use(express.static(publicPath));
 
 
 
-app.listen(3000 , () => {
+server.listen(port , () => {
     console.log(`App started on port ${port}`);
 })
 
