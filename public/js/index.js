41f53c3fe2039c5f423a050cfdb211720a16b519
Harshit Khanna <harshitkhanna3254@gmail.com>
2018-05-12 19:09:45 +0530

    add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,26 @@
+var socket = io(); //we are making a req from client to server to open up a web socket and keep it open.
+        
+        socket.on('connect', () => {
+                console.log("Connected to server");
+
+                socket.emit('createMessage',{
+
+                    to: 'jenny',
+                    text: "ssup nigga? ... via createMessage"
+                })
+        });
+
+        socket.on('disconnect' , () => {
+            console.log("Disconnected from server");
+        })
+
+
+        socket.on('newMessage', (message) => {
+            console.log("New Message received from server. The details are : ", message) //this is the object and will contain the data send from server side and we are sending it as second arguement. 
+        })
+
+        // socket.on('', () => {
+
+        // })
+
+        
--- server/server.js
+++ server/server.js
@@ -16,12 +16,23 @@ app.use(express.static(publicPath));
 
 
 io.on('connection', (socket) => {
-console.log("New User Connected");
+  console.log("New User Connected");
+
+    socket.emit('newMessage', {
+        from: 'Sugandh Goyal',
+        text:'Yo Nigger!',
+        createdAt: 911
+    }); //Emitting a new event. Listened by client. Not a listener so not providing a callback like in client side
+        //Also, we can trigget this event without sending any data. To send data, we  just add another arguement which wil be an object
 
 
-socket.on('disconnect', () => {
-console.log("User was disconnected")
-})
+    socket.on('createMessage', (message) => {
+        console.log("New message received from client to server. The details are :: ",message);
+    })
+
+  socket.on('disconnect', () => {
+  console.log("User was disconnected")
+ })
 });
 
 server.listen(port , () => {
