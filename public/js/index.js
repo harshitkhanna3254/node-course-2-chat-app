526d7c58d575a911caa2b40ded8799a89614aab3
Harshit Khanna <harshitkhanna3254@gmail.com>
2018-06-26 23:01:10 +0530

    First prototype of chat app

--- public/js/index.js
+++ public/js/index.js
@@ -2,22 +2,7 @@ var socket = io(); //we are making a req from client to server to open up a web 
         
         socket.on('connect', () => {
                 console.log("Connected to server");
-
-                // socket.emit('createMessage',{
-
-                //     from: 'jenny',
-                //     text: "ssup nigga? ... via createMessage"
-                // })
         });
-        
-        //Down below is a stupid way I tried. I made 2 different socket.on() while I already had a newMessage() listener. Could've made the event as 'newMessage' only
-        // socket.on('selfjoining', () => {
-        //     console.log("Welcome to the Chat App,  User");
-        // })
-
-        // socket.on('otherjoining', () => {
-        //     console.log("Other user has joined the chat app.")
-        // })
 
         socket.on('disconnect' , () => {
             console.log("Disconnected from server");
@@ -25,10 +10,28 @@ var socket = io(); //we are making a req from client to server to open up a web 
 
         socket.on('newMessage', (message) => {
             console.log("New Message received from server. The details are : \n", message) //this is the object and will contain the data send from server side and we are sending it as second arguement. 
+            
+            var li = jQuery('<li></li>');
+            li.text(`${message.from} : ${message.text}`);
+
+            jQuery('#message-list').append(li);
         })
 
         // socket.emit('createMessage', {
-        //     from: 'createMessage User',
-        //     text: "To Everyone!"
+        //     from: 'Frank',
+        //     text: "Hi"
+        // }, (data) => {
+        //     console.log("Got it.",data)
         // })
         
+
+        jQuery('#message-form').on('submit', (e) => {
+            e.preventDefault(); // we have prevented default behaviour with this
+
+            socket.emit('createMessage', {
+                from: 'User',
+                text: jQuery('[name=message]').val()
+            }, () => {
+
+            })
+        });
--- server/server.js
+++ server/server.js
@@ -17,23 +17,16 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log("New User Connected");
-    // socket.emit('newMessage', {
-    //     from: 'Sugandh Goyal',
-    //     text:'Yo Nigger!',
-    //     createdAt: 911
-    // }); //Emitting a new event. Listened by client. Not a listener so not providing a callback like in client side
-    //     //Also, we can trigget this event without sending any data. To send data, we  just add another arguement which wil be an object
-
 
     socket.emit('newMessage', generateMessage('Admin','Welcome to the Chat App!'));
     socket.broadcast.emit('newMessage', generateMessage('Admin','New User has just joined.'))
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message,callback) => {
         console.log("New message received from client to server. The details are : ", message);
 
-        // io.emit('newMessage',generateMessage(message.from,message.text)) // socket.emit() is for a single socket. io.emit() isfor every socket/window/user.
-
-        socket.broadcast.emit('newMessage', generateMessage(message.from,message.text))
+         io.emit('newMessage',generateMessage(message.from,message.text)) // socket.emit() is for a single socket. io.emit() isfor every socket/window/user.
+         callback('This is from the server');
+        // socket.broadcast.emit('newMessage', generateMessage(message.from,message.text))
 
         //broadcast is an object. It has it's own emit() function. It wil emit the event to everyone but that socket.
         // socket.broadcast.emit('newMessage',{
